8ed3cacb5088b34477badb266bfa145bdc77e12e
Malcolm <malcolm5964@gmail.com>
2024-07-18 14:25:56 +0800

    w

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\malco\Documents\SIT\Y2T3\SSD\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\malco\\Documents\\SIT\\Y2T3\\SSD\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
