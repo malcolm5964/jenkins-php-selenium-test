55029d440e93373be2aef9cc1a67dc97179732c7
Malcolm <malcolm5964@gmail.com>
2024-07-21 13:20:42 +0800

    g

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -x
 docker rm -f my-apache-php-app || true
 docker run -d -p 80:80 --name my-apache-php-app -v ${WORKSPACE}/src:/var/www/html php:7.2-apache
-sleep 1
+sleep 5
 CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-apache-php-app)
 set +x
 
