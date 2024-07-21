2923b41272b028ba82ef572b012d822411b68031
Malcolm <malcolm5964@gmail.com>
2024-07-21 15:22:58 +0800

    h

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 
 set -x
-docker rm -f my-apache-php-app || true
 "C:\Users\malco\Documents\SIT\Y2T3\SSD\jenkins-php-selenium-test"
 docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\malco\\Documents\\SIT\\Y2T3\\SSD\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 5
-CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-apache-php-app)
 set +x
 
 echo 'Now...'
-echo "Visit http://${CONTAINER_IP} to see your PHP application in action."
+echo "Visit http://localhost to see your PHP application in action."
 
