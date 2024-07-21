8c370435d1326e68f4d033b8b613cba901ec7be0
Malcolm <malcolm5964@gmail.com>
2024-07-21 18:23:52 +0800

    h

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+echo "Current working directory:"
+pwd
+echo "Contents of current directory:"
+ls -la
+
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v "$(pwd)/src":/var/www/html php:7.2-apache
 sleep 10
